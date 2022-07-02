bb736b239d08e54fa81ea34877dce822bd2f537f
Marvin Blum <marvin.blum@emvi.com>
2022-07-02 15:56:17 +0200

    Added support for single access tokens, added new option to configure a header when behind a proxy or load balancer, updated SDK, updated screenshots.

--- pirsch-wordpress.php
+++ pirsch-wordpress.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Pirsch Analytics
  * Plugin URI:        https://pirsch.io/
  * Description:       Connect your Wordpress website to Pirsch Analytics.
- * Version:           1.2.0
+ * Version:           1.3.0
  * Requires at least: 5.2
  * Requires PHP:      7.4
  * Author:            Emvi Software GmbH
@@ -23,12 +23,14 @@ function pirsch_analytics_deactivate() {
 	delete_option('pirsch_analytics_hostname');
 	delete_option('pirsch_analytics_client_id');
 	delete_option('pirsch_analytics_client_secret');
+	delete_option('pirsch_analytics_header');
 }
 
 function pirsch_analytics_settings_page_init() {
 	register_setting('pirsch_analytics_page', 'pirsch_analytics_hostname');
 	register_setting('pirsch_analytics_page', 'pirsch_analytics_client_id');
 	register_setting('pirsch_analytics_page', 'pirsch_analytics_client_secret');
+	register_setting('pirsch_analytics_page', 'pirsch_analytics_header');
 	add_settings_section(
 		'pirsch_analytics_page',
 		__('Settings', 'pirsch_analytics'),
@@ -56,10 +58,18 @@ function pirsch_analytics_settings_page_init() {
 		'pirsch_analytics_page',
 		'pirsch_analytics_page'
 	);
+	add_settings_field(
+		'pirsch_analytics_header',
+		__('Header', 'pirsch_analytics'),
+		'pirsch_analytics_header_callback',
+		'pirsch_analytics_page',
+		'pirsch_analytics_page'
+	);
 }
 
 function pirsch_analytics_settings_callback() {
-	echo '<p>Use the hostname you configured on the Pirsch dashboard for your website (e.g. example.com). To gain a client ID and secret, navigate to the Pirsch dashboard, click on settings, and create a new client. Read our <a href="https://docs.pirsch.io/get-started/backend-integration/" target="_blank">backend integration</a> for details.</p>';
+	echo '<p>Use the hostname you configured on the Pirsch dashboard for your website (e.g. example.com). To gain a client ID and secret, navigate to the Pirsch dashboard, click on settings, and create a new client. You can also use a single access token and skip the client ID. Read our <a href="https://docs.pirsch.io/get-started/backend-integration/" target="_blank">backend integration</a> for details.</p>';
+	echo '<p>The header is optional and should only be set when WordPress is running behind a proxy or load balancer. Pirsch requires the real visitor IP address, so you must provide the right header. Options are: CF-Connecting-IP, True-Client-IP, X-Forwarded-For, Forwarded, X-Real-IP.</p>';
 }
 
 function pirsch_analytics_hostname_callback() {
@@ -70,6 +80,11 @@ function pirsch_analytics_hostname_callback() {
 function pirsch_analytics_client_id_callback() {
 	$hostname = get_option('pirsch_analytics_client_id', '');
 	echo '<input type="text" name="pirsch_analytics_client_id" value="'.esc_attr($hostname).'" />';
+}
+
+function pirsch_analytics_header_callback() {
+	$header = get_option('pirsch_analytics_header', '');
+	echo '<input type="text" name="pirsch_analytics_header" value="'.esc_attr($header).'" />';
 }
 
 function pirsch_analytics_client_secret_callback() {
@@ -117,10 +132,33 @@ function pirsch_analytics_middleware() {
 			$hostname = get_option('pirsch_analytics_hostname');
 			$clientID = get_option('pirsch_analytics_client_id');
 			$clientSecret = get_option('pirsch_analytics_client_secret');
+			$header = get_option('pirsch_analytics_header');
 
-			if(!empty($hostname) && !empty($clientID) && !empty($clientSecret)) {
+			if(!empty($hostname) && !empty($clientSecret)) {
 				$client = new Pirsch\Client($clientID, $clientSecret, $hostname);
-				$client->hit();
+				$options = new Pirsch\HitOptions();
+
+				if(!empty($header)) {
+					switch(strtolower($header)) {
+						case 'cf-connecting-ip':
+							$options->ip = parseXForwardedFor($_SERVER['HTTP_CF_CONNECTING_IP']);
+							break;
+						case 'true-client-ip':
+							$options->ip = parseXForwardedFor($_SERVER['HTTP_TRUE_CLIENT_IP']);
+							break;
+						case 'x-forwarded-for':
+							$options->ip = parseXForwardedFor($_SERVER['HTTP_X_FORWARDED_FOR']);
+							break;
+						case 'forwarded':
+							$options->ip = parseForwardedHeader($_SERVER['HTTP_FORWARDED']);
+							break;
+						case 'x-real-ip':
+							$options->ip = $_SERVER['HTTP_X_REAL_IP'];
+							break;
+					}
+				}
+
+				$client->pageview($options);
 			}
 		}
 	} catch(Exception $e) {
@@ -132,6 +170,38 @@ function pirsch_analytics_is_wp_site() {
 	$pattern = "/\/*wp-.*/i";
 	$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 	return preg_match($pattern, $path) === 1;
+}
+
+function parseXForwardedFor($header) {
+	$parts = explode(',', $header);
+	$n = count($parts);
+
+	if($n > 0) {
+		return $parats[$n-1];
+	}
+
+	return '';
+}
+
+function parseForwardedHeader($header) {
+	$parts = explode(',', $header);
+	$n = count($parts);
+
+	if($n > 0) {
+		$parts = explode(';', $parts[$n-1]);
+
+		foreach($parts as $part) {
+			$kv = explode('=', $part, 1);
+
+			if(count($kv) == 2 && $kv[0] == 'for') {
+				return $kv[1];
+			}
+		}
+
+		unset($part);
+	}
+
+	return '';
 }
 
 register_activation_hook(__FILE__, 'pirsch_analytics_activate');
